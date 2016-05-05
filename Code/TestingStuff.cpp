dc7346b57b9cb5149817e949e9d9e2352d0173d7
Shaun <sdmsinclair@gmail.com>
2016-05-05 15:18:34 +1200

    motor test working

--- Code/TestingStuff.cpp
+++ Code/TestingStuff.cpp
@@ -4,14 +4,16 @@
 # include <stdio.h>
 # include <time.h>
 
-extern "C" int InitHardware();
+extern "C" int init();
 extern "C" int Sleep(int sec, int usec);
 extern "C" int set_motor(int motor , int speed );
 
 int main() {
-    InitHardware();
+    init();
     set_motor(1,255);
+    set_motor(2,255);
     Sleep(5,0);
     set_motor(1,0);
+    set_motor(2,0);
     return 0;
 }
