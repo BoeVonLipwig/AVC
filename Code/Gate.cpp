e6eca820266c3afe381c4c39ce6199b57049c922
Shaun <sdmsinclair@gmail.com>
2016-05-05 15:48:46 +1200

    adding gate opening code to makefile - Rachel

--- Code/Gate.cpp
+++ Code/Gate.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-# include <stdlib.h>
-# include <stdio.h>
-# include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 extern "C" int init(int d_lev);
 extern "C" int connect_to_server(char server_addr[15], int port);
@@ -15,7 +15,7 @@ int main() {
     char gateIp[] = "130.195.6.196";
     char msg[] = "my-password";
     connect_to_server(gateIp, 22);
-    pass=receive_from_server(msg);
+    pass = receive_from_server(msg);
     send_to_server((char *) pass);
     return 0;
 }
