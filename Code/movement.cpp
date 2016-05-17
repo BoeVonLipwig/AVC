ffb89a2a0b8e1f2bf4c4a7d615524866ad1a8f82
Lenton <lentonneale@gmail.com>
2016-05-17 23:30:13 +0000

    adjusted kp and implemented count

--- Code/movement.cpp
+++ Code/movement.cpp
@@ -13,7 +13,7 @@ int main() {
     int previous_error = 0;
     while(true) {
         take_picture();
-        float kp = 0.125; //proportional constant (might need to be changed later based on testing)
+        float kp = 0.5; //proportional constant (might need to be changed later based on testing)
         float ki = 0; //integral constant
         float kd = 0; //derivatve constant
         
@@ -25,18 +25,19 @@ int main() {
         int error_period = 1; //try to get a default number by testing (currently set to 1 to avoid dividing by 0)
         int error_diff = 0;
         int whiteBlack = 0;
-        //int count =0;
+        int count =0;
         for(int i = 0; i < 320; i++) {
             if(get_pixel(i, 120, 3) > 127) { //to compare the brightness
                 whiteBlack = 1; //white
-                //count++;
+                count++;
             }
             else {
                 whiteBlack = 0; //black
             }
             current_error += (i - 160) * whiteBlack; //help determine scale of adjustment
         }
-        //current_error/count;
+	if(count > 0)
+        	current_error /= count;
         total_error += current_error; //the sum of all errors
         
       	proportional_signal = current_error * kp;
@@ -46,8 +47,8 @@ int main() {
       	derivative_signal = (error_diff/error_period) * kd;
       	
       	previous_error = current_error;
-      	//printf("proportional signal: %d\n", proportional_signal);
-        set_motor(1, 32 + (proportional_signal + integral_signal + derivative_signal)); //might need smaller speed to help testing
-        set_motor(2, 32 - (proportional_signal + integral_signal + derivative_signal));
+      	printf("proportional signal: %d\n", proportional_signal);
+        //set_motor(1, 32 + (proportional_signal + integral_signal + derivative_signal)); //might need smaller speed to help testing
+        //set_motor(2, 32 - (proportional_signal + integral_signal + derivative_signal));
     }
 }
