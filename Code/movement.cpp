0d1a5fef71327c85e69608ab986d4b923f2a42d4
Lenton <lentonneale@gmail.com>
2016-05-18 04:47:46 +0000

    fiddling with integral response

--- Code/movement.cpp
+++ Code/movement.cpp
@@ -15,7 +15,7 @@ int main() {
     while(true) {
         take_picture();
         float kp = -0.1; //proportional constant (might need to be changed later based on testing)
-        float ki = 0; //integral constant
+        float ki = 0.1; //integral constant
         float kd = 0; //derivatve constant
 
         float  proportional_signal = 0;
@@ -48,8 +48,8 @@ int main() {
       	derivative_signal = (error_diff/error_period) * kd;
 
       	previous_error = current_error;
-//      	printf("proportional signal: %d\n", proportional_signal);
-		set_motor(1, base_speed + (proportional_signal + integral_signal + derivative_signal)); //might need smaller speed to help testing
-		set_motor(2, base_speed - (proportional_signal + integral_signal + derivative_signal)); 
+	printf("integral signal: %d\n", integral_signal);
+	//set_motor(1, base_speed + (proportional_signal + integral_signal + derivative_signal)); //might need smaller speed to help testing
+	//set_motor(2, base_speed - (proportional_signal + integral_signal + derivative_signal)); 
     }
 }
